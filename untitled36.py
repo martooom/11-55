c02cc7a76acc34f55cadf6b5ad757e9c230d3d77
martooom <martinriveracastillo92@gmail.com>
2024-11-22 12:07:24 -0300

    Update untitled36.py

--- untitled36.py
+++ untitled36.py
@@ -1,23 +1,14 @@
 import streamlit as st
-import base64
-
-# Función para convertir una imagen a base64
-def image_to_base64(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode()
 
 # Ruta de la imagen (asegúrate de que "valorant.jpg" esté en /content/)
 image_fondo = "valorant.jpg"
-
-# Convertir la imagen a base64
-image_base64 = image_to_base64(image_fondo)
 
 # Código CSS para establecer la imagen como fondo usando base64
 st.markdown(
     f"""
     <style>
     body {{
-        background-image: url('data:image_fondo;base64,{image_base64}');
+        background-image: url('data:image_fondo,{image_base64}');
         background-size: cover;
         background-position: center;
         height: 100vh;
