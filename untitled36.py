6ebe066c70a57feddae15093d8e526d3f3cc96cd
martooom <martinriveracastillo92@gmail.com>
2024-11-22 12:10:44 -0300

    Update untitled36.py

--- untitled36.py
+++ untitled36.py
@@ -8,7 +8,7 @@ st.markdown(
     f"""
     <style>
     body {{
-        background-image: url('data:image_fondo,{image_valorant}');
+        background-image: url('data:image_fondo,{image_fondo}');
         background-size: cover;
         background-position: center;
         height: 100vh;
